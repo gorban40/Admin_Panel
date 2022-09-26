c1a40beaeef5413a2a35ee604fcb3f329ce85073
Misha Gorban <mixei8189@gmail.com>
2022-09-26 11:30:18 +0300

    problem

--- gulpfile.js
+++ gulpfile.js
@@ -4,57 +4,57 @@ const sass = require('gulp-sass')(require('sass'));
 
 const dist = 'D:/Programming/OpenServer/domains/books/admin';
 
-gulp.task('copy-html', () => {
-    return gulp.src('./app/src/index.html')
-        .pipe(gulp.dest(dist));
+gulp.task("copy-html", () => {
+    return gulp.src("./app/src/index.html")
+                .pipe(gulp.dest(dist));
 });
 
 gulp.task("build-js", () => {
     return gulp.src("./app/src/main.js")
-        .pipe(webpack({
-            mode: 'development',
-            output: {
-                filename: 'script.js'
-            },
-            watch: false,
-            devtool: "source-map",
-            module: {
-                rules: [
-                    {
-                        test: /\.m?js$/,
-                        exclude: /(node_modules|bower_components)/,
-                        use: {
-                            loader: 'babel-loader',
-                            options: {
+                .pipe(webpack({
+                    mode: 'development',
+                    output: {
+                        filename: 'script.js'
+                    },
+                    watch: false,
+                    devtool: "source-map",
+                    module: {
+                        rules: [
+                          {
+                            test: /\.m?js$/,
+                            exclude: /(node_modules|bower_components)/,
+                            use: {
+                              loader: 'babel-loader',
+                              options: {
                                 presets: [['@babel/preset-env', {
                                     debug: true,
                                     corejs: 3,
                                     useBuiltIns: "usage"
                                 }],
-                                    "@babel/react"]
+                                 "@babel/react"]
+                              }
                             }
-                        }
-                    }
-                ]
-            }
-        }))
-        .pipe(gulp.dest(dist));
+                          }
+                        ]
+                      }
+                }))
+                .pipe(gulp.dest(dist));
 });
 
 gulp.task("build-sass", () => {
-    return gulp.src('./app/scss/style.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest(dist));
+    return gulp.src("./app/scss/style.scss")
+                .pipe(sass().on('error', sass.logError))
+                .pipe(gulp.dest(dist));
 });
 
 gulp.task("copy-api", () => {
-    return gulp.src('./app/api/**/*.*')
-        .pipe(gulp.dest(dist + "/api"));
+    return gulp.src("./app/api/**/*.*")
+                .pipe(gulp.dest(dist + "/api"));
 });
 
 gulp.task("copy-assets", () => {
-    return gulp.src('./app/assets/**/*.*')
-        .pipe(gulp.dest(dist + "/assets"));
+    return gulp.src("./app/assets/**/*.*")
+                .pipe(gulp.dest(dist + "/assets"));
 });
 
 gulp.task("watch", () => {
